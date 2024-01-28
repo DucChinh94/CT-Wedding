bf9e40c1f10e98ecdbeb024eb447e49a08e630fd
ChinhND <ducchinhhd94@gmail.com>
2024-01-28 13:24:22 +0700

    up project

---
+++ js/thoviet.js
@@ -0,0 +1,15 @@
+(function ($){
+    $(document).ready(function(){
+        let slider_content = `
+        <span>WE’RE GETTING MARRIED</span>
+        <h4>Save Our Date</h4>
+        <p>25 February 2024</p>
+        `
+        $('.slider-content--inner').html(slider_content)
+    })
+})(jQuery)
+
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    console.log('Shaadi me zaroor aana');
+});
